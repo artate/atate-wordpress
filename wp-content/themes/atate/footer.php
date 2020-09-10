4930c5745ad2bccbb3710d1b17f8327f0caed4e3
artate <at@atate.co.uk>
2020-09-10 17:56:47 +0100

    Change from using gulp to webpack. File name and directory changes.

--- wp-content/themes/atate/footer.php
+++ wp-content/themes/atate/footer.php
@@ -3,8 +3,8 @@
 			<div class="row">
 				<div class="col-12">
 					<p class="mr-2">ATATE © <?php echo date('Y'); ?> </p> |
-					<p class="ml-2"><a href="https://www.linkedin.com/in/andrew-tate-14100b62/"><i class="fa fa-linkedin"></i></a></p>
-					<p class="ml-2"><a href="https://github.com/artate"><i class="fa fa-github"></i></a></p>
+					<p class="ml-2"><a href="https://www.linkedin.com/in/andrew-tate-14100b62/" target="_blank"><i class="fa fa-linkedin"></i></a></p>
+					<p class="ml-2"><a href="https://github.com/artate" target="_blank"><i class="fa fa-github"></i></a></p>
 				</div>
 			</div>
 		</div>
@@ -13,7 +13,7 @@
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/main.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/assets/build/js/app.js"></script>
 
 
 		<?php wp_footer(); ?>
